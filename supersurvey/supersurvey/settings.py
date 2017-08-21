23d7eea428bf2b959c4bcb378dce55e4c88300e7
Kevin Khanda <kevin.khanda@gmail.com>
2017-08-21 22:04:02 +0300

    Establish PostgreSQL db

--- supersurvey/supersurvey/settings.py
+++ supersurvey/supersurvey/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'supersurvey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'supersurveydb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': 'supersurveytestdb',
+        },
     }
 }
 

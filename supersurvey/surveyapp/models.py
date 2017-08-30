a4cebe237e22948d26b4ad3c3e450727172e289b
Khanda Kevin <kevin.khanda@gmail.com>
2017-08-30 18:42:36 +0300

    Add models and changes in admin interface

--- supersurvey/surveyapp/models.py
+++ supersurvey/surveyapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+CHOICE_TYPE = (
+    ('MC', 'Multiple Choice'),
+    ('NR', 'Numerical Response'),
+    ('TE', 'Text Entry'),
+)
+
+
+class Question(models.Model):
+    text = models.TextField()
+    variants = ArrayField(models.CharField(max_length=256))
+    type = models.CharField(max_length=2, choices=CHOICE_TYPE)
+    number = models.IntegerField()
+    deleted = models.BooleanField(default=False)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    session_id = models.IntegerField()
+    answer = models.TextField()

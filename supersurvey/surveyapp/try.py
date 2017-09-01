33a3bd7c8bc2d4d19d3f82ae4f20d5c7b7ed9776
Your Name <sergey_umnov@mail.ru>
2017-09-01 14:11:50 +0300

    Add try.py files with survey aggregated results querying

---
+++ supersurvey/surveyapp/try.py
@@ -0,0 +1,20 @@
+nr_Questions = Question.objects.filter(type='NR', deleted=False)
+averages = []
+for q in nr_Questions:
+	nr_Answers = Answers.objects.filter(question=q)
+	count = 0
+	total = 0
+	for ans in nr_Answers:
+		count += 1
+		total += int(ans.answer)
+	average = (q.id, total/count)
+	averages.append(average)
+
+def mc_question_distribution(id):
+	q = Question.get(pk=id)
+	mc_Answers = Question.objects.filter(question=q)
+	distribution = []
+	for i, variant in enumerate(question.variants):
+		count = mc_Answers.filter(answer=variant).count()
+		distribution.append((i, variant, count))
+	return distribution

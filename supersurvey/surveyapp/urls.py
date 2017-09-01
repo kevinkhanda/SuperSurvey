d28adb0180e2bf1e0e49858dc575426d1fa56016
Khanda Kevin <kevin.khanda@gmail.com>
2017-09-01 12:31:25 +0300

    Add new views

--- supersurvey/surveyapp/urls.py
+++ supersurvey/surveyapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^hello', views.hello, name="hello")
+    url(r'^hello/$', views.hello, name='hello'),
+    url(r'^questions/$', views.questions, name='questions'),
+    url(r'^questions/(?P<question_id>[0-9]+)/details/$', views.question_details, name='details')
 ]
--- supersurvey/surveyapp/views.py
+++ supersurvey/surveyapp/views.py
@@ -9,3 +9,13 @@ def index(request):
 
 def hello(request):
     return render(request, "hello.html", {'message': 'message from view'})
+
+
+def questions(request):
+    response = "Here is the list of questions"
+    return HttpResponse(response)
+
+
+def question_details(request, question_id):
+    response = "You are looking at details of question %s."
+    return HttpResponse(response % question_id)

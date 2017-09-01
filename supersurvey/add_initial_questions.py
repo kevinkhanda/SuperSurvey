587caf2ac52a4ceebf32013ce0cb8e1d1214b2e2
Alik Khilazhev <alikhil@mail.ru>
2017-09-01 16:24:57 +0300

    Add init questions script

---
+++ supersurvey/add_initial_questions.py
@@ -0,0 +1,22 @@
+from surveyapp.models import Question
+
+Question.objects.create(
+    text='What is your favorite programming language',
+    type='MC',
+    number=1,
+    variants=['C#', 'Java', 'Python', 'Go', 'Pascal', 'C++']
+).save()
+
+Question.objects.create(
+    text='How many years do you write code?',
+    type='NR',
+    number=2,
+    variants=[],
+).save()
+
+Question.objects.create(
+    text='Where are you from?',
+    type='TE',
+    number=3,
+    variants=[],
+).save()
